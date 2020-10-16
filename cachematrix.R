dcea1941cafa0446ce07f4ab6232c6168cfbcded
Jan Gil <jgsarmiento1@up.edu.ph>
2020-10-16 21:40:44 +0800

    Added functions for creating a cache for the inverse of a matrix, and for retrieving the inverse of the matrix input

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a special matrix used to cache
+## the inverse of a matrix "x"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Computes for the inverse of the special matrix stored by makeCacheMatrix 
+## function above. If the inverse has already been calculated, then this function
+## retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+## Testing the functions by getting the inverse of the matrix below
+eh <- makeCacheMatrix(matrix(c(4,6,2,8),2,2))
+eh$getinv()
+cacheSolve(eh)
+
+
+## Making sure that the inverse is correct, by getting the inverse 
+## of the inverse which is the original matrix input
+oh <- makeCacheMatrix(cacheSolve(eh))
+oh$getinv()
+cacheSolve(oh)
